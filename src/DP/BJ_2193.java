61f115eff9338e3595d66d17e58184d5634f4b1c
choiyeeun1010 <yeeun201010@naver.com>
2022-02-12 01:53:31 +0900

    [백준] 2193 : 이친수

--- src/DP/BJ_2193.java
+++ src/DP/BJ_2193.java
@@ -5,15 +5,19 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class BJ_2193 {
-    public static void main(String args[]) throws IOException{
+    public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
-        long d[] = new long[count+1];
-        d[0] = 0;
-        d[1] = 1;
-        for (int i = 2; i <= count; i++){
-            d[i] = d[i-1] + d[i-2];
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        System.out.println(d[count]);
+
+        System.out.println(dp[n]);
     }
 }
