bcfaa1943880108440ba78d5147f316bae7f16a0
choiyeeun1010 <yeeun201010@naver.com>
2022-02-12 03:10:18 +0900

    [백준] 2156 : 포도주 시식

--- src/DP/BJ_2156.java
+++ src/DP/BJ_2156.java
@@ -5,29 +5,25 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class BJ_2156 {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N + 1];
-        int[] dp = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
+        for(int i = 1; i <= n; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         dp[1] = arr[1];
-        if (N > 1) {
+        if(n > 1){
             dp[2] = arr[1] + arr[2];
         }
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
 
+        for(int i = 3; i <= n; i++){
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
         }
-        System.out.println(dp[N]);
-    }
 
+        System.out.println(dp[n]);
+    }
 }
