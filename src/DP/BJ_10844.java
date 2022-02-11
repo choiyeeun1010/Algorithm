28b01678998cf42a60220907b7d8e8d1b45d53d9
choiyeeun1010 <yeeun201010@naver.com>
2022-02-11 22:45:30 +0900

    [백준] 10844 : 쉬운 계단 수

--- src/DP/BJ_10844.java
+++ src/DP/BJ_10844.java
@@ -1,34 +1,40 @@
 package DP;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class BJ_10844 {
-  static int N; 
-  static final long MOD = 1_000_000_000L; 
-  public static void main(String[] args) throws IOException {
-    
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-    N = Integer.parseInt(bf.readLine()); long[][] dp = new long[N+1][10]; 
+  final static long mod = 1000000000;
+  public static void main(String[] args) throws IOException{
 
-    dp[1][0] = 0L; 
-    for(int i=1; i<10; i++) { 
-      dp[1][i] = 1L; 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    long[][] dp = new long[n+1][10];
+
+    for(int i = 1; i < 10; i++){
+      dp[1][i] = 1;
     }
 
-    for(int i=2; i<=N; i++) { 
-      dp[i][0] = dp[i-1][1]; 
-      
-      for(int j=1; j<=9; j++) { 
-        if(j == 9) { dp[i][9] = dp[i-1][8] % MOD; } 
-        else dp[i][j] = (dp[i-1][j-1] % MOD + dp[i-1][j+1] % MOD) % MOD; 
-      } 
-    } 
-    
-    long result = 0; 
-    for(int i=0; i<10; i++) { 
-      result = (result + dp[N][i]) % MOD; 
-    } 
-    System.out.print(result); 
-  } 
+    for(int i = 2; i <= n; i++){
+      for(int j = 0; j < 10; j++){
+        if(j == 0)
+          dp[i][0] = dp[i-1][1] % mod;
+        else if(j == 9)
+          dp[i][9] = dp[i-1][8] % mod;
+        else
+          dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+      }
+    }
+
+    long result = 0;
+
+    for(int i = 0; i < 10; i++){
+      result += dp[n][i];
+    }
+
+    System.out.println(result % mod);
+
+  }
 }
