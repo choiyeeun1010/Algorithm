d9ae81a764ff14d4f3b39347c1f11b1bd132670c
choiyeeun1010 <yeeun201010@naver.com>
2022-02-13 23:10:44 +0900

    [백준] 11053 : 가장 긴 증가하는 부분 수열

---
+++ src/DP/BJ_11053.java
@@ -0,0 +1,38 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BJ_11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
+
+            for(int j = 0; j <= i; j++){
+                if(arr[j] < arr[i] && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = -1;
+        for(int i = 0; i < n; i++){
+            max = dp[i] > max ? dp[i] : max;
+        }
+        System.out.println(max);
+    }
+}
