56f0ca72f37feca2df574b6c3f1504ad22487e28
choiyeeun1010 <yeeun201010@naver.com>
2022-02-14 00:08:44 +0900

    [백준] 11055 : 가장 큰 증가 부분 수열

---
+++ src/DP/BJ_11055.java
@@ -0,0 +1,35 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BJ_11055 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        int max = -1;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+            dp[i] = arr[i];
+
+            for(int j = 0; j <= i; j++){
+                if(arr[j] < arr[i] && dp[i] < dp[j] + arr[i])
+                    dp[i] = dp[j] + arr[i];
+            }
+
+            if(max < dp[i]) max = dp[i];
+        }
+        System.out.println(max);
+    }
+}
